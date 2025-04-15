271123a2b71a6817dd1b987a61931797bfd84836
HSANTOSSS <hugosantos1.hs@gmail.com>
2025-04-15 15:19:07 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white p-10">
+      <h1 className="text-4xl font-bold mb-4">CryptoRadar</h1>
+      <p className="text-lg">O hub definitivo do universo cripto</p>
+    </div>
+  );
+}
