f4f39014bded1a85de5aa2f022e2f01f556d068a
HSANTOSSS <hugosantos1.hs@gmail.com>
2025-04-15 14:51:36 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,3 @@
+
+document.getElementById("crypto-prices").innerText = "Exemplo: BTC - $65,000 | ETH - $3,200";
+document.getElementById("news-feed").innerText = "Última notícia: Mercado cripto atinge novo pico em 2025.";
